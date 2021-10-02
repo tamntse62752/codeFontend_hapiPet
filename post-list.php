83e2d6ece23bc903efc1bbcdd530bad70ac9cad6
Thanh Tam Nguyen <simon.ag2107@gmail.com>
2021-10-02 17:14:31 +0700

    Add Image for post

---
+++ post-list.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pets - Pet Care, Shop, and Veterinary Html Template</title>
+	
+	<!-- ==============================================
+	Favicons
+	=============================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	
+	<!-- ==============================================
+	CSS VENDOR
+	=============================================== -->
+	<link rel="stylesheet" type="text/css" href="css/vendor/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/vendor/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/animate.min.css">
+	
+	<!-- ==============================================
+	Custom Stylesheet
+	=============================================== -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/customeStyle.css" />
+    <script src="js/vendor/modernizr.min.js"></script>
+
+</head>
+
+<body>
+
+	<!-- LOAD PAGE -->
+	<div class="animationload">
+		<div class="loader"></div>
+	</div>
+	
+	<!-- BACK TO TOP SECTION -->
+	<a href="#0" class="cd-top cd-is-visible cd-fade-out">Top</a>
+
+	<!-- HEADER -->
+    <div class="header header-1">
+
+		<!-- TOP BAR -->
+    	<div class="topbar d-none d-md-block">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-sm-6 col-md-6">
+						<p class="mb-0">Welcome to The Best Pets Care at Melboune</p>
+					</div>
+
+					<div class="col-sm-6 col-md-6">
+						<div class="sosmed-icon d-inline-flex pull-right">
+							<a href="#"><i class="fa fa-facebook"></i></a> 
+							<a href="#"><i class="fa fa-twitter"></i></a> 
+							<a href="#"><i class="fa fa-instagram"></i></a> 
+							<a href="#"><i class="fa fa-pinterest"></i></a> 
+						</div>
+					</div>
+
+					
+				</div>
+			</div>
+		</div>
+
+		<!-- MIDDLE BAR -->
+		<div class="middlebar d-none d-sm-block">
+			<div class="container">
+				
+				
+				<div class="contact-info">
+					<!-- INFO 1 -->
+					<div class="box-icon-1">
+						<div class="icon">
+							<div class="fa fa-clock-o"></div>
+						</div>
+						<div class="body-content">
+							<div class="heading">Open Hours :</div>
+							Mon - Fri : 09:00 - 20:00
+						</div>
+					</div>
+					<!-- INFO 2 -->
+					<div class="box-icon-1">
+						<div class="icon">
+							<div class="fa fa-phone"></div>
+						</div>
+						<div class="body-content">
+							<div class="heading">Call Today :</div>
+							+62 7100 1234
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<!-- NAVBAR SECTION -->
+		<div class="navbar-main stiky">
+			<div class="container">
+				<nav id="navbar-example" class="navbar navbar-expand-lg">
+					<a class="navbar-brand" href="index.html">
+						<img src="https://i.imgur.com/Tqj8m5J.png" alt="">
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul class="navbar-nav">
+							<li class="nav-item dropdown dmenu">
+								<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TRANG CHỦ</a>
+								<div class="dropdown-menu">
+									<a class="dropdown-item" href="index.html">Pets Care</a>
+									<a class="dropdown-item" href="shop.html">Pets Shop</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="accountUpgrade.html">Upgrade account</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="findPet.php">Find Pet</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="mypet.html">Your Pet</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="post-list.php">Post List</a>
+							</li>
+							
+						</ul>
+						<a id="modal_trigger" class="btn btn-secondary btn-nav btn-rect ml-auto">Liên hệ ngay</a>
+					</div>
+				</nav> <!-- -->
+
+			</div>
+		</div>
+
+    </div>
+
+	<!-- BANNER -->
+	<div class="section banner-page" data-background="images/dummy-img-1920x300.jpg">
+		<div class="content-wrap pos-relative">
+			<div class="d-flex justify-content-center bd-highlight mb-2">
+				<div class="title-page">Blog List</div>
+			</div>
+			<p class="text-center text-white">Your pet's health and well-being are our top priority.</p>
+		</div>
+	</div>
+
+	<!-- BREADCRUMB -->
+	<div class="section bg-breadcrumb">
+		<div class="content-wrap py-0 pos-relative">
+			<div class="container">
+			    <nav aria-label="breadcrumb">
+				  <ol class="breadcrumb ">
+				    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+				    <li class="breadcrumb-item active" aria-current="page">Post List</li>
+				  </ol>
+				</nav>					
+			</div>
+		</div>
+	</div>
+
+	<!-- CONTENT -->
+	<div class="content clearfix">
+
+      <!-- Main Content -->
+      <div class="main-content"> 
+
+        <div class="post clearfix">
+          <img src="images/post/10_phut.jpg" alt="" class="post-image">
+          <div class="post-preview">
+            <h5><a href="single-post.html">10 phut huan luyen cho moi ngay</a></h5>
+            <i class="far fa-user"> Tam Nguyen</i>
+            &nbsp;
+            <i class="far fa-calendar"> 11/10/2021</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="images/post/thay_rang.jpg" alt="" class="post-image">
+          <div class="post-preview">
+            <h5><a href="single-post.html">Cho trong giai doan thay rang</a></h5>
+            <i class="far fa-user"> Tam Nguyen</i>
+            &nbsp;
+            <i class="far fa-calendar"> 11/10/2021</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="images/post/dinh_duong.jpg" alt="" class="post-image">
+          <div class="post-preview">
+            <h5><a href="single-post.html">Cac chat dinh duong can phai biet</a></h5>
+            <i class="far fa-user"> Tam Nguyen</i>
+            &nbsp;
+            <i class="far fa-calendar"> 11/10/2021</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="images/post/soc_nhiet.jpg" alt="" class="post-image">
+          <div class="post-preview">
+            <h5><a href="single-post.html">Chong soc nhiet cho thu cung</a></h5>
+            <i class="far fa-user"> Tam Nguyen</i>
+            &nbsp;
+            <i class="far fa-calendar"> 11/10/2021</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            
+          </div>
+        </div>
+        
+
+      </div>
+      <!-- // Main Content -->
+
+      <div class="sidebar">
+
+        <div class="section search">
+          <h5 class="section-title">Search</h5>
+          <form action="index.html" method="post">
+            <input type="text" name="search-term" class="text-input" placeholder="Search...">
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+
+	<!-- CTA -->
+	
+
+	<!-- FOOTER SECTION -->
+	<div class="footer bg-overlay-secondary" data-background="images/dummy-img-1920x900-3.jpg">
+		<div class="content-wrap">
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-sm-6 col-md-4">
+						<div class="footer-item">
+							<img src="images/logo_w.png" alt="logo bottom" class="logo-bottom">
+							<div class="spacer-20"></div>
+							<p>We are pets center at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.</p>
+							<div class="spacer-20"></div>
+							<img src="images/payment.png" alt="">
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+						<div class="footer-item">
+							<div class="footer-title">
+								Opening Hours
+							</div>
+							<p>Our support available to help you 24 hours a day. We provide our best.</p>
+							<ul class="list">
+								<li>
+									Mon - Fri : 08.00 am - 20.00 pm
+								</li>
+								<li>
+									Saturday : 09.00 am - 20.00 pm
+								</li>
+								<li>
+									Sunday :  We Are Closed
+								</li>
+							</ul>
+
+						</div>
+					</div>
+					
+					<div class="col-sm-6 col-md-4">
+						<div class="footer-item">
+							<div class="footer-title">
+								Contact Info
+							</div>
+							<p>Lit sed The Best in dolor sit amet consectetur</p>
+							<ul class="list-info">
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-map-marker"></span>
+									</div>
+									<div class="info-text">99 S.t Jomblo Park Pekanbaru 28292. Indonesia</div> 
+								</li>
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-phone"></span>
+									</div>
+									<div class="info-text">(0761) 654-123987</div>
+								</li>
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-envelope"></span>
+									</div>
+									<div class="info-text">mail@hellopets.com</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="fcopy">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+						<p class="ftex">&copy; 2019 Your Company. All Rights Reserved. Designed By <span class="text-primary">Rometheme</span></p> 
+					</div>
+					<div class="col-sm-6 col-md-6">
+						<div class="sosmed-icon d-inline-flex float-right">
+							<a href="#"><i class="fa fa-facebook"></i></a> 
+							<a href="#"><i class="fa fa-twitter"></i></a> 
+							<a href="#"><i class="fa fa-instagram"></i></a> 
+							<a href="#"><i class="fa fa-pinterest"></i></a> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+	<!-- JS VENDOR -->
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/vendor/owl.carousel.js"></script>
+	<script src="js/vendor/jquery.magnific-popup.min.js"></script>
+
+	<!-- SENDMAIL -->
+	<script src="js/vendor/validator.min.js"></script>
+	<script src="js/vendor/form-scripts.js"></script>
+
+	<script src="js/script.js"></script>
+
+</body>
+</html>
