d7fd78d18343332779d36ddf2cd65309c569af80
Thanh Tam Nguyen <simon.ag2107@gmail.com>
2021-10-06 21:18:12 +0700

    update post background

--- post-list.php
+++ post-list.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <!-- Basic Page Needs
-    ================================================== -->
-    <meta charset="utf-8">
-    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pets - Pet Care, Shop, and Veterinary Html Template</title>
-	
+    	================================================== -->
+    	<meta charset="utf-8">
+    	<!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    		<meta name="viewport" content="width=device-width, initial-scale=1">
+    		<title>Pets - Pet Care, Shop, and Veterinary Html Template</title>
+    		
 	<!-- ==============================================
 	Favicons
 	=============================================== -->
@@ -32,7 +32,7 @@
 	=============================================== -->
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<link rel="stylesheet" type="text/css" href="css/customeStyle.css" />
-    <script src="js/vendor/modernizr.min.js"></script>
+	<script src="js/vendor/modernizr.min.js"></script>
 
 </head>
 
@@ -47,10 +47,10 @@
 	<a href="#0" class="cd-top cd-is-visible cd-fade-out">Top</a>
 
 	<!-- HEADER -->
-    <div class="header header-1">
+	<div class="header header-1">
 
 		<!-- TOP BAR -->
-    	<div class="topbar d-none d-md-block">
+		<div class="topbar d-none d-md-block">
 			<div class="container">
 				<div class="row align-items-center">
 					<div class="col-sm-6 col-md-6">
@@ -118,7 +118,7 @@
 								<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TRANG CHỦ</a>
 								<div class="dropdown-menu">
 									<a class="dropdown-item" href="index.html">Pets Care</a>
-									<a class="dropdown-item" href="shop.html">Pets Shop</a>
+									<a class="dropdown-item" href="shop.php">Pets Shop</a>
 								</div>
 							</li>
 							<li class="nav-item">
@@ -128,10 +128,7 @@
 								<a class="nav-link" href="findPet.php">Find Pet</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="mypet.html">Your Pet</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="post-list.php">Blog List</a>
+								<a class="nav-link" href="post-list.php">Pet Blog</a>
 							</li>
 							
 						</ul>
@@ -142,15 +139,15 @@
 			</div>
 		</div>
 
-    </div>
+	</div>
 
 	<!-- BANNER -->
-	<div class="section banner-page" data-background="images/dummy-img-1920x300.jpg">
+	<div class="section banner-page" data-background="images/post/post_bg.jpg">
 		<div class="content-wrap pos-relative">
 			<div class="d-flex justify-content-center bd-highlight mb-2">
 				<div class="title-page">Blog List</div>
 			</div>
-			<p class="text-center text-white">Your pet's health and well-being are our top priority.</p>
+			<p class="text-center text-white">Cap nhat thong tin tung ngay</p>
 		</div>
 	</div>
 
@@ -158,161 +155,161 @@
 	<div class="section bg-breadcrumb">
 		<div class="content-wrap py-0 pos-relative">
 			<div class="container">
-			    <nav aria-label="breadcrumb">
-				  <ol class="breadcrumb ">
-				    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-				    <li class="breadcrumb-item active" aria-current="page">Blog List</li>
-				  </ol>
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb ">
+						<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Blog List</li>
+					</ol>
 				</nav>					
-			</div>
+			</div>	
 		</div>
 	</div>
 
 	<!-- CONTENT -->
 	<div class="content clearfix">
 
-      <!-- Main Content -->
-      <div class="main-content"> 
+		<!-- Main Content -->
+		<div class="main-content"> 
 
-        <div class="post clearfix">
-          <img src="images/post/10_phut.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h3><a href="single-post.html">10 phut huan luyen cho moi ngay</a></h3>
-            <i class="far fa-user"> Tam Nguyen</i>
-            &nbsp;
-            <i class="far fa-calendar"> 11/10/2021</i>
-            <p class="preview-text">
-              Chó là 1 loài vật rất trung thành với con người. Chúng là loài vật đáng yêu và mang lại nhiều niềm vui cho con người. Tuy vậy để huấn luyện chó con biết vâng lời thì cần phải có 1 sự nỗ lực và kiên trì từ bạn
-            </p>
-            
-          </div>
-        </div>
-        <div class="post clearfix">
-          <img src="images/post/thay_rang.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h3><a href="single-post.html">Cho trong giai doan thay rang</a></h3>
-            <i class="far fa-user"> Tam Nguyen</i>
-            &nbsp;
-            <i class="far fa-calendar"> 11/10/2021</i>
-            <p class="preview-text">
-              Những chú chó nói riêng và tất cả thú cưng nhìn chung đều rất trung thành. Chúng bảo vệ bạn khỏi người lạ, chào đón bạn sau một ngày dài mệt mỏi và luôn thể hiện tình cảm với bạn.
-            </p>
-            
-          </div>
-        </div>
-        <div class="post clearfix">
-          <img src="images/post/dinh_duong.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h3><a href="single-post.html">Cac chat dinh duong can phai biet</a></h3>
-            <i class="far fa-user"> Tam Nguyen</i>
-            &nbsp;
-            <i class="far fa-calendar"> 11/10/2021</i>
-            <p class="preview-text">
-              Những chú chó nói riêng và tất cả thú cưng nhìn chung đều rất trung thành. Chúng bảo vệ bạn khỏi người lạ, chào đón bạn sau một ngày dài mệt mỏi và luôn thể hiện tình cảm với bạn.
-            </p>
-            
-          </div>
-        </div>
-        <div class="post clearfix">
-          <img src="images/post/soc_nhiet.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h3><a href="single-post.html">Chong soc nhiet cho thu cung</a></h3>
-            <i class="far fa-user"> Tam Nguyen</i>
-            &nbsp;
-            <i class="far fa-calendar"> 11/10/2021</i>
-            <p class="preview-text">
-              Chó bị sốc nhiệt là tình trạng nhiệt độ cơ thể chó cao quá mức (thân nhiệt của chó đo tại trực tràng dao động từ 40.5 tới 43 độ C) gây ra những bất thường trong hệ thống thần kinh và dẫn tới những tình trạng như hôn mê, yếu ớt, ngất xỉu hay lờ đờ.
-            </p>
-            
-          </div>
-        </div>
-        
+			<div class="post clearfix">
+				<img src="images/post/10_phut.jpg" alt="" class="post-image">
+				<div class="post-preview">
+					<h3><a href="single-post.php">10 phut huan luyen cho moi ngay</a></h3>
+					<i class="far fa-user"> Tam Nguyen</i>
+					&nbsp;
+					<i class="far fa-calendar"> 11/10/2021</i>
+					<p class="preview-text">
+						Chó là 1 loài vật rất trung thành với con người. Chúng là loài vật đáng yêu và mang lại nhiều niềm vui cho con người. Tuy vậy để huấn luyện chó con biết vâng lời thì cần phải có 1 sự nỗ lực và kiên trì từ bạn
+					</p>
+					
+				</div>
+			</div>
+			<div class="post clearfix">
+				<img src="images/post/thay_rang.jpg" alt="" class="post-image">
+				<div class="post-preview">
+					<h3><a href="single-post.php">Cho trong giai doan thay rang</a></h3>
+					<i class="far fa-user"> Tam Nguyen</i>
+					&nbsp;
+					<i class="far fa-calendar"> 11/10/2021</i>
+					<p class="preview-text">
+						Những chú chó nói riêng và tất cả thú cưng nhìn chung đều rất trung thành. Chúng bảo vệ bạn khỏi người lạ, chào đón bạn sau một ngày dài mệt mỏi và luôn thể hiện tình cảm với bạn.
+					</p>
+					
+				</div>
+			</div>
+			<div class="post clearfix">
+				<img src="images/post/dinh_duong.jpg" alt="" class="post-image">
+				<div class="post-preview">
+					<h3><a href="single-post.php">Cac chat dinh duong can phai biet</a></h3>
+					<i class="far fa-user"> Tam Nguyen</i>
+					&nbsp;
+					<i class="far fa-calendar"> 11/10/2021</i>
+					<p class="preview-text">
+						Những chú chó nói riêng và tất cả thú cưng nhìn chung đều rất trung thành. Chúng bảo vệ bạn khỏi người lạ, chào đón bạn sau một ngày dài mệt mỏi và luôn thể hiện tình cảm với bạn.
+					</p>
+					
+				</div>
+			</div>
+			<div class="post clearfix">
+				<img src="images/post/soc_nhiet.jpg" alt="" class="post-image">
+				<div class="post-preview">
+					<h3><a href="single-post.php">Chong soc nhiet cho thu cung</a></h3>
+					<i class="far fa-user"> Tam Nguyen</i>
+					&nbsp;
+					<i class="far fa-calendar"> 11/10/2021</i>
+					<p class="preview-text">
+						Chó bị sốc nhiệt là tình trạng nhiệt độ cơ thể chó cao quá mức (thân nhiệt của chó đo tại trực tràng dao động từ 40.5 tới 43 độ C) gây ra những bất thường trong hệ thống thần kinh và dẫn tới những tình trạng như hôn mê, yếu ớt, ngất xỉu hay lờ đờ.
+					</p>
+					
+				</div>
+			</div>
+			
 
-      </div>
-      <!-- // Main Content -->
+		</div>
+		<!-- // Main Content -->
 
-      <div class="sidebar">
-        <div class="section search">
-          <h3 class="section-title">Search</h3>
-          <form action="index.html" method="post">
-            <input type="text" name="search-term" class="text-input" placeholder="Search...">
-          </form>
-        </div>
+		<div class="sidebar">
+			<div class="section search">
+				<h3 class="section-title">Search</h3>
+				<form action="index.html" method="post">
+					<input type="text" name="search-term" class="text-input" placeholder="Search...">
+				</form>
+			</div>
 
-      </div>
+		</div>
 
-    </div>
+	</div>
 
 
 	<!-- FOOTER SECTION -->
 	<div class="footer bg-overlay-secondary" data-background="https://i.imgur.com/IolBwzT.png" style="background-image: url(&quot;https://i.imgur.com/IolBwzT.png&quot;);">
-						<div class="content-wrap">
-							<div class="container">
+		<div class="content-wrap">
+			<div class="container">
 
-								<div class="row">
-									<div class="col-sm-6 col-md-4">
-										<div class="footer-item">
-											<img src="https://i.imgur.com/WjN1HZX.png" alt="logo bottom" class="logo-bottom">
-											<div class="spacer-20"></div>
-											<p>Hapi Pet mang đến cho người dùng một cộng đồng những người yêu thích thú cưng. Ở đây các bạn có thể nhận được dịch vụ chăm sóc tốt nhất và cùng nhau chia sẻ những khoảnh khắc đáng nhớ về thú cưng của mình.</p>
-										</div>
-									</div>
-
-									<div class="col-sm-6 col-md-4">
-
-									</div>
-
-									<div class="col-sm-6 col-md-4">
-										<div class="footer-item">
-											<div class="footer-title">
-												Liên hệ
-											</div>
-											<p>Công ty Tâm Huyết Việt</p>
-											<ul class="list-info">
-												<li>
-													<div class="info-icon text-primary">
-														<span class="fa fa-map-marker" aria-hidden="true"></span>
-													</div>
-													<div class="info-text">449 Trần Hưng Đạo, Q1, Tp HCM</div> 
-												</li>
-												<li>
-													<div class="info-icon text-primary">
-														<span class="fa fa-phone" aria-hidden="true"></span>
-													</div>
-													<div class="info-text">090 6979 xxx</div>
-												</li>
-												<li>
-													<div class="info-icon text-primary">
-														<span class="fa fa-envelope" aria-hidden="true"></span>
-													</div>
-													<div class="info-text">hapipet@gmail.com</div>
-												</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
+				<div class="row">
+					<div class="col-sm-6 col-md-4">
+						<div class="footer-item">
+							<img src="https://i.imgur.com/WjN1HZX.png" alt="logo bottom" class="logo-bottom">
+							<div class="spacer-20"></div>
+							<p>Hapi Pet mang đến cho người dùng một cộng đồng những người yêu thích thú cưng. Ở đây các bạn có thể nhận được dịch vụ chăm sóc tốt nhất và cùng nhau chia sẻ những khoảnh khắc đáng nhớ về thú cưng của mình.</p>
 						</div>
+					</div>
 
-						<div class="fcopy">
-							<div class="container">
-								<div class="row">
-									<div class="col-sm-6 col-md-6">
-										<p class="ftex">© 2021 Designed By <span class="text-primary">Do Nam</span></p> 
-									</div>
-									<div class="col-sm-6 col-md-6">
-										<div class="sosmed-icon d-inline-flex float-right">
-											<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a> 
-											<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a> 
-											<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a> 
-											<a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a> 
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+					<div class="col-sm-6 col-md-4">
 
 					</div>
+
+					<div class="col-sm-6 col-md-4">
+						<div class="footer-item">
+							<div class="footer-title">
+								Liên hệ
+							</div>
+							<p>Công ty Tâm Huyết Việt</p>
+							<ul class="list-info">
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-map-marker" aria-hidden="true"></span>
+									</div>
+									<div class="info-text">449 Trần Hưng Đạo, Q1, Tp HCM</div> 
+								</li>
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-phone" aria-hidden="true"></span>
+									</div>
+									<div class="info-text">090 6979 xxx</div>
+								</li>
+								<li>
+									<div class="info-icon text-primary">
+										<span class="fa fa-envelope" aria-hidden="true"></span>
+									</div>
+									<div class="info-text">hapipet@gmail.com</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="fcopy">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+						<p class="ftex">© 2021 Designed By <span class="text-primary">Do Nam</span></p> 
+					</div>
+					<div class="col-sm-6 col-md-6">
+						<div class="sosmed-icon d-inline-flex float-right">
+							<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a> 
+							<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a> 
+							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a> 
+							<a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
 	
 	<!-- JS VENDOR -->
 	<script src="js/vendor/jquery.min.js"></script>
